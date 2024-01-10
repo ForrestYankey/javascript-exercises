d97821fd6b74383934d0fc0dead3bdf2082bc416
ForrestYankey <forrestyankey@gmail.com>
2024-01-10 13:49:32 -0500

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let number = parseInt(num);
+    
+    if (num < 0) {
+        return 'OOPS'
+    } 
 
-};
+    if (number === 1 || number === 2) {
+        return 1;
+    }
+    
+    let fib = 0;
+    let num1 = 1;
+    let num2 = 1;
+
+    for (let i = 2; i < number; i++) {
+        fib = num1 + num2;
+        num1 = num2;
+        num2 = fib;
+    }
+    
+    return fib;
+    }
 
 // Do not edit below this line
 module.exports = fibonacci;
