571922c1f8c2754c7b56f039ef6bfab15a5caedb
ForrestYankey <forrestyankey@gmail.com>
2024-01-10 14:28:11 -0500

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function([...people]) {
+    
+    const oldestPerson = people.reduce((oldest, person) => {
+        const currentDate = new Date().getFullYear();
+        const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentDate - person.yearOfBirth;
+        if(!oldest.age || age > oldest.age ) {
+            oldest.name = person.name;
+            oldest.age = age;
+        }
+        return oldest;
+    }, {})
 
+    return oldestPerson;
 };
 
 // Do not edit below this line
