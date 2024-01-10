57279e1a7baa368df237c390f45f73f5e621f325
ForrestYankey <forrestyankey@gmail.com>
2024-01-10 12:40:28 -0500

    calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((multiply, num) => multiply * num);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let result = num;
+	for (let i = 1; i < power; i++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  let number = num;
+  for (let i = 1; i < num; i++) {
+    total *= number;
+    number--;
+  }
+  return total;
 };
 
 // Do not edit below this line
