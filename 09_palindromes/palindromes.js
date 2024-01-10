fb44bb8d03abc091d49e13e9e7511dec96ec2de0
ForrestYankey <forrestyankey@gmail.com>
2024-01-10 13:15:01 -0500

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (characters) {
+    const lowerCase = characters.toLowerCase();
+    const array = Array.from(lowerCase);
+    const cleanArray = array.filter(char => char != '!' && 
+                                            char != ',' &&
+                                            char != '.' &&
+                                            char != ' '
+                                            );
+    const length = cleanArray.length - 1;
+    let isPalindrome = true;
 
+    for (let i = 0; i < Math.floor(length / 2); i++) {
+        let currentFront = cleanArray[i];
+        let currentBack = cleanArray[(length) - i];
+        if (currentFront !== currentBack) {
+            isPalindrome = false;
+            break;
+        }  
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
